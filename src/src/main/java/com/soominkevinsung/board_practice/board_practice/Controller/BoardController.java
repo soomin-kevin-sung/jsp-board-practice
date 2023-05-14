4c982302602565d509368d79e13f05c2c341ed91
KevinSung <ssm0725@gmail.com>
2023-05-15 02:41:27 +0900

    add hello template and mapping

---
+++ src/src/main/java/com/soominkevinsung/board_practice/board_practice/Controller/BoardController.java
@@ -0,0 +1,16 @@
+package com.soominkevinsung.board_practice.board_practice.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        // 템플릿 경로 (templates)
+        return "/boards/hello";
+    }
+}
